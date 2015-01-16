22cf9566fba38c0cbf8e0fafa71066a96255fa7b
Kaizeras <hgeorgievkzr@gmail.com>
2015-01-16 22:08:55 +0200

    qwd

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,7 +10,7 @@ Personalwebsite::Application.configure do
   # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
   config.assets.compile = true
-  config.assets.precompile =  ['*.js', '*.css', '*.css.erb']
+  config.assets.precompile =  ['*.js']
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
