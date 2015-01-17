425931b4d1d9abae38cf9c67f2d40b54f7f48c04
Kaizeras <hgeorgievkzr@gmail.com>
2015-01-17 03:21:25 +0200

    asd

--- app/assets/javascripts/post-nav-control.js
+++ app/assets/javascripts/post-nav-control.js
@@ -3,10 +3,11 @@ $(document).ready(function(){
       if( $(".toprow").scrollTop() == 0 )  {
        $(".tab-bar").removeClass("no-top-border");
        $(".tab-bar").addClass("top-border");
+       $(".title-header").show();
       } else {
         $(".tab-bar").addClass("no-top-border");
        $(".tab-bar").removeClass("top-border");
-        
+        $(".title-header").hide();
       }
     });
 })
