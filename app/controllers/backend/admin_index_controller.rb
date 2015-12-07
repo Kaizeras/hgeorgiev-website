e764004be1376efe3e599527bbb27364e40ab90d
Hristo Georgiev <hgeorgievkzr@gmail.com>
2015-12-07 11:40:01 +0200

    update to 2015

--- app/controllers/backend/admin_index_controller.rb
+++ app/controllers/backend/admin_index_controller.rb
@@ -1,5 +1,5 @@
 class Backend::AdminIndexController < Backend::ApplicationController
-    before_action :authenticate_admin!
+  before_action :authenticate_admin!
   def index
     @posts = Post.all;
   end
--- app/controllers/backend/application_controller.rb
+++ app/controllers/backend/application_controller.rb
@@ -1,5 +1,5 @@
 class Backend::ApplicationController < ActionController::Base
-
+   before_action :authenticate_admin!
    layout 'admin'
    protect_from_forgery
 end
